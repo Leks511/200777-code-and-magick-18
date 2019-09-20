1f23af150ff270b8a64d488178acf6953238b4f7
Александр <leks511@bk.ru>
2019-09-20 23:47:58 +0300

    Сделал часть второго задания, добавил функции

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,28 @@
-window.renderStatistics = function(ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+'use strict';
 
-  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.fillRect(100, 10, 420, 270);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var TEXT_COLOR = 'rgba(0, 0, 0, 0.7)';
 
-  ctx.font = '16px "PT Mono"';
-  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-  ctx.fillText('Ура вы победили!', 120, 50);
-  ctx.fillText('Список результатов:', 120, 70);
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT)
+}
+
+var renderText = function (ctx, font, text, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.fillText(text, x, y);
   ctx.textBaseline = 'hanging';
+}
+
+
+window.renderStatistics = function(ctx, names, times) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, 'rgba(255, 255, 255, 1)');
+
+  renderText(ctx, '16px "PT Mono"', 'Ура вы победили!', 120, 50, TEXT_COLOR);
+  renderText(ctx, '16px "PT Mono"', 'Список результатов:', 120, 65, TEXT_COLOR);
 
   var maxValue = times[0];
 
@@ -20,7 +33,5 @@ window.renderStatistics = function(ctx, names, times) {
   }
 
   maxValue = Math.floor(maxValue);
-
-  console.log(maxValue);
 
 }
