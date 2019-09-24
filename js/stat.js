ee99f5ad01e03c2c3aab6d8b82d7866d938b2971
Александр <leks511@bk.ru>
2019-09-24 23:37:35 +0300

    Внёс правки по ответу наставника

--- js/stat.js
+++ js/stat.js
@@ -2,11 +2,13 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_COLOR = 'rgba(255, 255, 255, 1)';
 
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
 
+var YOURSELF_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
@@ -14,6 +16,16 @@ var BAR_Y = 100;
 
 var TEXT_HEIGHT = 20;
 var TEXT_GAP = BAR_GAP;
+var FONT = '16px "PT Mono"';
+
+var FIRST_COLOR = 'rgba(0, 0, 0, 0.7)';
+var SECOND_COLOR = 'rgba(0, 0, 0, 0.7)';
+
+var CONGRATULATIONS = 'Ура вы победили!';
+var INTRO_WORD = 'Список результатов:';
+
+var TOP_X_GAP = 120;
+var TOP_TEXT_Y = 30;
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -21,8 +33,8 @@ var renderCloud = function(ctx, x, y, color) {
 }
 
 var renderText = function (ctx, text, x, y) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.font = '16px "PT Mono"';
+  ctx.fillStyle = FIRST_COLOR;
+  ctx.font = FONT;
   ctx.fillText(text, x, y);
   ctx.textBaseline = 'hanging';
 }
@@ -32,10 +44,6 @@ var getSaturation = function() {
 }
 
 var getMaxElement = function(arr) {
-  if (!(arr.length)) {
-    return false;
-  }
-
   var maxValue = arr[0];
 
   if (arr.length === 1) {
@@ -54,13 +62,11 @@ var getMaxElement = function(arr) {
 
 window.renderStatistics = function(ctx, names, times) {
 
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, FIRST_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  renderText(ctx, 'Ура вы победили!', 120, 40);
-  renderText(ctx, 'Список результатов:', 120, 45);
-
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  renderText(ctx, CONGRATULATIONS, TOP_X_GAP, TOP_TEXT_Y);
+  renderText(ctx, INTRO_WORD, TOP_X_GAP, TOP_TEXT_Y + TEXT_HEIGHT);
 
   var maxTime = getMaxElement(times);
 
@@ -80,7 +86,7 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.fillStyle = 'hsl(240, ' + saturationValue + ', 50%)'
 
     if (playerName === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = YOURSELF_BAR_COLOR;
     }
 
     ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * playerIndex, barY, BAR_WIDTH, barHeight);
