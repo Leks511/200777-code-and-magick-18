37382be0c97f39bb3c56ed88f1a881d89f29758e
Александр <leks511@bk.ru>
2019-10-03 20:30:03 +0300

    Добавь перетаскивание окна персонажа, удали самопроизводный показ окна настройки персонажа

--- js/setup.js
+++ js/setup.js
@@ -7,8 +7,6 @@ var QUANTITY_OF_WIZARDS = 4;
 
 var setupWindow = document.querySelector('.setup');
 
-setupWindow.classList.remove('hidden');
-
 function getRandomIndex(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -238,4 +236,55 @@ wizardFireball.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     changeWizardFireballColor();
   }
+});
+
+
+var setupWindowHandler = document.querySelector('.upload');
+
+setupWindowHandler.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  function onMouseMove(moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setupWindow.style.top = (setupWindow.offsetTop - shift.y) + 'px';
+    setupWindow.style.left = (setupWindow.offsetLeft - shift.x) + 'px';
+  }
+
+  function onMouseUp(upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      function onClickPreventDefault(evt) {
+        evt.preventDefault();
+        setupWindowHandler.removeEventListener('click', onClickPreventDefault);
+      }
+      setupWindowHandler.addEventListener('click', onClickPreventDefault);
+    }
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
 });
